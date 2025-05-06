9ec46ed938ddd0c99c53fdcc3d370a45da9de9e0
Willy Nojopranoto <nojopranoto@gmail.com>
2025-05-06 17:56:11 +0000

    Fix comments

--- federated_search/deploy.sh
+++ federated_search/deploy.sh
@@ -8,32 +8,32 @@ gcloud config set project "${PROJECT_ID}"
 
 # PD
 
-# gcloud builds submit ./amp-pd-service \
-#   --tag "us-central1-docker.pkg.dev/${PROJECT_ID}/docker-repo/amp-pd"
+gcloud builds submit ./amp-pd-service \
+  --tag "us-central1-docker.pkg.dev/${PROJECT_ID}/docker-repo/amp-pd"
 
-# gcloud run deploy amp-pd \
-#   --image "us-central1-docker.pkg.dev/${PROJECT_ID}/docker-repo/amp-pd" \
-#   --platform managed \
-#   --region us-central1 \
-#   --allow-unauthenticated \
-#   --set-env-vars "DATABASE_URL=sqlite:///./pd.db"
+gcloud run deploy amp-pd \
+  --image "us-central1-docker.pkg.dev/${PROJECT_ID}/docker-repo/amp-pd" \
+  --platform managed \
+  --region us-central1 \
+  --allow-unauthenticated \
+  --set-env-vars "DATABASE_URL=sqlite:///./pd.db"
 
 # AD
 
-# gcloud builds submit ./amp-ad-service \
-#   --tag "us-central1-docker.pkg.dev/${PROJECT_ID}/docker-repo/amp-ad"
+gcloud builds submit ./amp-ad-service \
+  --tag "us-central1-docker.pkg.dev/${PROJECT_ID}/docker-repo/amp-ad"
 
-# gcloud run deploy amp-ad \
-#   --image "us-central1-docker.pkg.dev/${PROJECT_ID}/docker-repo/amp-ad" \
-#   --platform managed \
-#   --region us-central1 \
-#   --allow-unauthenticated \
-#   --set-env-vars "DATABASE_URL=sqlite:///./ad.db"
+gcloud run deploy amp-ad \
+  --image "us-central1-docker.pkg.dev/${PROJECT_ID}/docker-repo/amp-ad" \
+  --platform managed \
+  --region us-central1 \
+  --allow-unauthenticated \
+  --set-env-vars "DATABASE_URL=sqlite:///./ad.db"
 
 # sysbio
 
-# gcloud builds submit ./sysbio-service \
-#   --tag "us-central1-docker.pkg.dev/${PROJECT_ID}/docker-repo/sysbio"
+gcloud builds submit ./sysbio-service \
+  --tag "us-central1-docker.pkg.dev/${PROJECT_ID}/docker-repo/sysbio"
 
 gcloud run deploy sysbio \
   --image "us-central1-docker.pkg.dev/${PROJECT_ID}/docker-repo/sysbio" \
