6b9e36d011b624b3fe1b40cee3694f4872de8231
Willy Nojopranoto <nojopranoto@gmail.com>
2025-05-07 13:26:08 -0700

    Have the app also serve the toy client (#13)

--- federated_search/sysbio-service/main.py
+++ federated_search/sysbio-service/main.py
@@ -1,15 +1,25 @@
-from fastapi import FastAPI
-from app.routes import federated_search
+from fastapi import FastAPI, Request
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
+from app.routes import federated_search
+from pathlib import Path
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5500"],
+    allow_origins=["http://localhost:8000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+static_dir = Path(__file__).resolve().parent / "static"
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
+
+@app.get("/")
+async def serve_index():
+    return FileResponse(static_dir / "index.html")
 
 app.include_router(federated_search.router)
