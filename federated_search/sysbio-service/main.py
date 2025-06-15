2214775f19c53201ceb4cf9d3a2ce4ad6a4f88ec
Willy Nojopranoto <nojopranoto@gmail.com>
2025-06-15 04:02:55 +0000

    Updates such as id/secret from .env

--- federated_search/sysbio-service/main.py
+++ federated_search/sysbio-service/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8000"],
+    allow_origins=["http://localhost:8000", "http://localhost:8501"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
