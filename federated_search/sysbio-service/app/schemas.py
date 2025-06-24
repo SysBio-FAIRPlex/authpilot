467b99131d3fe9c2be7732b6e95cbfcf8c2a9eb8
Willy Nojopranoto <nojopranoto@gmail.com>
2025-06-24 16:51:52 -0700

    More implementation for pagination (#35)

--- federated_search/sysbio-service/app/schemas.py
+++ federated_search/sysbio-service/app/schemas.py
@@ -3,3 +3,5 @@ from typing import Union, Any, Dict, List, Optional
 class SearchRequest(BaseModel):
     query: str
     parameters: Optional[Union[List[Any], Dict[str, Any]]] = None
+    limit: Optional[int] = 10
+    offset: Optional[int] = 0
